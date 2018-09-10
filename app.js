edc4d4704991c1ed21bef11c5f7362ae7adff8dc
Luis Waderdorff <walderdorff@live.de>
2018-09-10 18:30:38 +0200

    Fertig

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 let missed = 0;
 const startGame = document.querySelector(".btn__reset");
+const resetButton = document.querySelector(".reset-button");
 const overlay = document.getElementById("overlay");
 const phrases = [
   "Hello World and people",
@@ -16,6 +17,7 @@ const phrases = [
 // Hide Overlay
 startGame.addEventListener( "click", () => {
   overlay.style.display = "none";
+  startGame.style.display = "none";
 });
 
 // Randomly chose a phrase from the phrases array and split that array into
@@ -56,16 +58,72 @@ function checkLetter (button) {
       correctLetter = letterList[i].textContent;
     }
   }
-  console.log(correctLetter);
   return correctLetter;
 }
 
+//Check if the player has won or lost
+function checkWin () {
+  const revealedLetters = document.querySelectorAll(".show");
+  const revealableLetters = document.querySelectorAll(".letter");
+  if ( revealedLetters.length === revealableLetters.length) {
+    overlay.style.display = "";
+    overlay.className = "win";
+    overlay.querySelector(".win-title").style.display = "block";
+    resetButton.style.display = "block";
+  } else if ( missed === 5) {
+    overlay.style.display = "";
+    overlay.className = "lose";
+    overlay.querySelector(".lost-title").style.display = "block";
+    resetButton.style.display = "block";
+  }
+}
+
 //Press the button to activate checkLetter
+let letterFound;
 qwerty.addEventListener( "click", (event) => {
   if ( event.target.tagName === "BUTTON") {
     event.target.classList.add("chosen");
     event.target.disabled = true;
-    const letterFound = checkLetter(event.target);
-    return letterFound;
+    letterFound = checkLetter(event.target);
+    if (letterFound === null) {
+      document.querySelector(".tries").firstElementChild.src = "images/lostHeart.png";
+      document.querySelector(".tries").className = "triesLost";
+      missed += 1;
+    }
+    checkWin();
   }
 });
+//Remove last phrases for Reset Game function
+function removeLastPhrase(){
+  const lastLetters = document.getElementsByClassName("letter");
+  const lastSpaces = document.getElementsByClassName("space");
+  while (lastLetters.length > 0) {
+      lastLetters[0].parentNode.removeChild(lastLetters[0]);
+      if (lastSpaces.length > 0) {
+        lastSpaces[0].parentNode.removeChild(lastSpaces [0]);
+      }
+  }
+}
+
+//Reset Game
+function resetGame () {
+  const chosenButtons = document.querySelectorAll(".keyrow button");
+  missed = 0;
+  //Refill hearts
+  while (document.querySelectorAll(".triesLost").length > 0) {
+    document.querySelectorAll(".triesLost")[0].firstElementChild.src = "images/liveHeart.png";
+    document.querySelectorAll(".triesLost")[0].className = "tries";
+  }
+ //Reset buttons
+  for (var i = 0; i < chosenButtons.length; i++) {
+    chosenButtons[i].className = "";
+    chosenButtons[i].disabled = false;
+  }
+  removeLastPhrase();
+  addPhraseToDisplay(phrases);
+}
+
+resetButton.addEventListener ("click", (event) => {
+  overlay.style.display = "none";
+  resetGame();
+})
